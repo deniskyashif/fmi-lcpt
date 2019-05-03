a3e1ee18a476f92dc12901155d93784e975f44e4
Denis Kyashif <denis.kyashif@gmail.com>
2019-05-03 11:16:51 -0700

    describe the Y factorial reduction

--- Y.clj
+++ Y.clj
@@ -24,11 +24,31 @@
 (def fact (Y1 fact-step))
 (fact 5)
 
+;; Factorial reduction using the Y combinator
+;; ------------------------------------------
+;; Y = λf.(λx.f (x x)) (λx.f (x x))
+;; fact-step = λf.λn.if (iszero n) 1 (mult (f (pred n) n))
+
+;; fact = Y fact-step = (λf.(λx.f (x x)) (λx.f (x x))) fact-step
+;;      -> (λx.fact-step (x x)) (λx.fact-step (x x))
+;;      -> fact-step ((λx.fact-step (x x)) (λx.fact-step (x x)))
+;;      == (λf.λn.if (iszero n) 1 (mult (f (pred n) n))) ((λx.fact-step (x x)) (λx.fact-step (x x)))
+;;      -> λn.(if (iszero n) 1 (mult ( (((λx.fact-step (x x)) (λx.fact-step (x x))) (pred n) ) n)))
+
+;; fact 1 == (λn.(if (iszero n) 1 (mult (((λx.fact-step (x x)) (λx.fact-step (x x))) (pred n)) n))) 1
+;; 		  ->      if (iszero 1) 1 (mult (((λx.fact-step (x x)) (λx.fact-step (x x))) (pred 1)) 1)
+;;        ->                       mult (((λx.fact-step (x x)) (λx.fact-step (x x))) 0) 1
+;;        ->                       mult     ((fact-step ((λx.fact-step (x x)) (λx.fact-step (x x)))) 0) 1
+;;        ==                       mult     ((λn.(if (iszero n) 1 (mult (((λx.fact-step (x x)) (λx.fact-step (x x))) (pred n) n)))) 0) 1
+;;        ->                       mult          (if (iszero 0) 1 (mult (((λx.fact-step (x x)) (λx.fact-step (x x))) (pred 0) 0))) 1
+;;        ->                       mult 1 1
+;;        ->                       1
+
 (defn fib-step [f]
   (fn [n]
     (if (<= n 1)
-      x
+      n
       (+ (f (- n 1)) (f (- n 2))))))
 
-(def fib (Y fib-step))
+(def fib (Y1 fib-step))
 (map fib (range 10))
