e3d665e800a33190cceea324b8f75510870a56b1
Denis Kyashif <denis.kyashif@gmail.com>
2019-05-13 22:47:08 +0300

    implement and test list reverse function

--- src/lists.clj
+++ src/lists.clj
@@ -25,7 +25,7 @@
             c-false))))
      c-true)))
 
-;; --- Test
+;; --- Test list construction
 (let [list012 ((pair 0) ((pair 1) ((pair 2) empty-list)))]
   (assert (= (is-empty empty-list) c-true))
   (assert (= (is-empty list012) c-false))
@@ -45,7 +45,7 @@
   ((fn [x] (fn [y] ((f (x x)) y)))
    (fn [x] (fn [y] ((f (x x)) y)))))
 
-(def append-step
+(def my-append-step
   (fn [next-step]
     (fn [list1]
       (fn [list2]
@@ -54,12 +54,12 @@
            (fn [] list2))
           (fn [] ((pair (head list1)) ((next-step (tail list1)) list2)))))))))
 
-(def append (Y1 append-step))
+(def my-append (Y1 my-append-step))
 
-;; --- Test
+;; --- Test my-append
 (let [l1 ((pair 0) empty-list)
       l2 ((pair 1) empty-list)
-      l12 ((append l1) l2)]
+      l12 ((my-append l1) l2)]
   (assert (= (head l12) 0))
   (assert (= (head (tail l12)) 1))
   (assert (= (is-empty (tail (tail l12)))) c-true))
@@ -69,18 +69,36 @@
   (fn [next-step]
     (fn [f]
       (fn [list]
+        ;; wrap both branches inside a function and call the result to simulate lazy evaluation
         ((((my-if (is-empty list))
           (fn [] list))
          (fn [] ((pair (f (head list))) ((next-step f) (tail list))))))))))
 
 (def my-map (Y1 my-map-step))
 
-;; --- Test
-(let [list012 ((pair 2) ((pair 3) ((pair 4) empty-list)))
-      squared012 ((my-map (fn [x] (* x x))) list012)]
+;; --- Test my-map
+(let [list234 ((pair 2) ((pair 3) ((pair 4) empty-list)))
+      squared234 ((my-map (fn [x] (* x x))) list234)]
   (assert (= ((my-map identity) empty-list)))  
-  (assert (= (head squared012) 4))
-  (assert (= (head (tail squared012))))
-  (assert (= (head (tail (tail squared012))))))
+  (assert (= (head squared234) 4))
+  (assert (= (head (tail squared234))))
+  (assert (= (head (tail (tail squared234))))))
 ;; ---
 
+(def my-reverse-step
+  (fn [next-step]
+    (fn [list]
+      ((((my-if (is-empty list))
+         (fn [] empty-list))
+        (fn []
+          ((my-append (next-step (tail list))) ((pair (head list)) empty-list))))))))
+
+(def my-reverse (Y1 my-reverse-step))
+
+;; --- Test my-reverse
+(let [l12 ((pair 1) ((pair 2) empty-list))
+      revl12 (my-reverse l12)]
+  (assert (= (head revl12) 2))
+  (assert (= (head (tail revl12)) 1))
+  (assert (= (my-reverse empty-list) empty-list)))
+;; ---
